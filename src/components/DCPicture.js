82ced8d8fe546cfa06d91378f918396ff8fc784e
Dr. Ulrich Anders <37266274+DrUlrichAnders@users.noreply.github.com>
2018-06-02 13:13:11 +0200

    Blurup for mobile too

--- src/components/DCPicture.js
+++ src/components/DCPicture.js
@@ -16,10 +16,11 @@ class DCPicture extends React.Component {
           media={"(min-width: 481px)"}
           srcSet={this.props.img["0100"]["16x9"]}
           data-srcset={this.props.img["0800"]["16x9"]}
-        />
+          />
         <Img
           data-sizes="100vw"
-          src={this.props.img["0480"]["16x9"]}
+          srcSet={this.props.img["0100"]["16x9"]}
+          data-srcset={this.props.img["0480"]["16x9"]}
           className="lazyload blur-up"
           alt={"Missing image"}
         />
