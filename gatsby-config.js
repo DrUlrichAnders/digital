04a9e2b03e1796c3c130c0ddf092e7d29b58207e
Dr. Ulrich Anders <37266274+DrUlrichAnders@users.noreply.github.com>
2018-07-19 11:43:31 +0200

    gatsby-netlify-plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-react-next",
     "gatsby-plugin-styled-components",
+    "gatsby-plugin-netlify",
     {
       resolve: `gatsby-plugin-typography`,
       options: {
