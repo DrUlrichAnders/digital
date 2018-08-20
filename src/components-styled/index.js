3b5722b1168139060696f09da9c6c35366836cf4
Dr. Ulrich Anders <37266274+DrUlrichAnders@users.noreply.github.com>
2018-08-20 18:05:06 +0200

    Remove Roboto

--- src/components-styled/index.js
+++ src/components-styled/index.js
@@ -107,7 +107,7 @@ export const LinkFooterStyled = styled(Link)`
 
 export const LogoBrand = styled.span`
   color: #fff;
-  font-family: "Roboto";
+  font-family: "Ubuntu";
   font-size: 2.3888889rem;
   line-height: 3rem;
   text-decoration: none;
